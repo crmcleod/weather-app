bdb6aaefc08444c94656a9613768c41f6417c868
Craig McLeod <mcleod.craig@outlook.com>
2020-11-02 11:04:26 +0000

    Able to change location, currently need to use go and re-render buttons

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -1,25 +1,51 @@
 import React, {useState, useEffect} from 'react'
+import DayCard from './DayCard'
+import MetCardToday from './MetCardToday'
 import MetOffice from './MetOffice'
 
 const MainContainer = () => {
 
     const [ locationInput, setLocationInput ] = useState('Edinburgh')
-    const [ todaysWeather, setTodaysWeather ] = useState()
+    const [ locationKey, setLocationKey ] = useState(3166)
+    const [ metLocations, setMetLocations ] = useState()
+    const [ todaysWeather, setTodaysWeather ] = useState([])
+    const [ locations, setLocations ] = useState()
 
     const handleLocationChange = (e) => {
-        setLocationInput(e.target.value)
+        setLocationInput(e.target.value)}
+
+    const handleSetKey = (func) => {
+        setLocationKey(locations[locations.findIndex(function(location) {
+            return location.location == locationInput })].id)
+
     }
 
+    let sortedLocationList;
+    if(!locations){
+    } else {
+        sortedLocationList = locations.map( location => {
+            return (
+                <option value={location.location} key={location.id}>{location.location}</option>)
+    })}
+
    const getTodaysWeather = (weather) => {
-        setTodaysWeather({...todaysWeather, weather})
+        setTodaysWeather(weather)
    }
 
     return (
         <>
             <h1> { locationInput } </h1>
-            <label htmlFor="location-input"> Choose location...</label>
-            <input id="location-input" type="text" value={locationInput} onChange={handleLocationChange} placeholder="Choose location..."/>
-            <MetOffice getTodaysWeather={getTodaysWeather} />
+            <label htmlFor="location-input"> Choose location...
+            <input list="location-input" onChange={handleLocationChange} placeholder="Edinburgh"></input>
+                <datalist id="location-input" value={locationInput}  placeholder="Choose location...">
+                    {sortedLocationList}
+                </datalist>
+                <button id="set-location-key" onClick={handleSetKey} >Go!</button>
+            </label>
+            <MetCardToday data={todaysWeather.metOfficeToday} />
+            <MetOffice
+            getTodaysWeather={getTodaysWeather} 
+            locations={locations} setLocations={setLocations} locationKey={locationKey} />
         </>
     )
 
--- src/MetOffice.js
+++ src/MetOffice.js
@@ -2,14 +2,36 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import DayCard from './DayCard'
 
-const MetOffice = ({getTodaysWeather}) => {
+const MetOffice = ({
+    getTodaysWeather, 
+    locations, setLocations, locationKey}) => {
 
-    const [weatherData, setWeatherData] = useState()
-    const [locationID, setLocationId] = useState(3166)
+    const [weatherData, setWeatherData] = useState();
+    const [locationID, setLocationId] = useState(locationKey);
 
-    const url = (locationID, apiKey) => `//datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${locationID}?res=3hourly&key=${apiKey}`
-    
-    let keyList, dayCards;
+
+    useEffect( async () => {
+        setLocationId(locationKey)
+        const url = (locationID, apiKey) => `//datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${locationID}?res=3hourly&key=${apiKey}`
+        const result = await axios( url(locationID, process.env.REACT_APP_API_KEY) )
+        const locations = await axios(url('sitelist', process.env.REACT_APP_API_KEY))
+
+        const adjustedLocations = locations.data.Locations.Location.map((location) => {
+            return (
+                {
+                    location: location.name,
+                    id: location.id
+                }
+            )
+        })
+
+        setWeatherData(result.data.SiteRep);
+        setLocations(adjustedLocations);
+        getTodaysWeather({metOfficeToday: result.data.SiteRep.DV.Location.Period[0].Rep[result.data.SiteRep.DV.Location.Period[0].Rep.map(function (e) {
+        return e.$ }).indexOf("720")]})
+    }, [locationID])
+
+    let dayCards;
     if(!weatherData){} else {
     dayCards = 
         weatherData.DV.Location.Period.map((day) => {
@@ -17,17 +39,9 @@ const MetOffice = ({getTodaysWeather}) => {
         })
     }
 
-    useEffect( async () => {
-        const result = await axios(
-            url(locationID, process.env.REACT_APP_API_KEY)
-        )
-        setWeatherData(result.data.SiteRep);
-        
-        getTodaysWeather(weatherData.DV.Location.Period[0].Rep[weatherData.DV.Location.Period[0].Rep.map(function (e) {
-            return e.$ }).indexOf("900")
-    ])
-    }, [])
-
+    const handleClick = () => {
+        setLocationId(locationKey)
+    }
     if(!weatherData){
         return(
             <p> ...loading</p>
@@ -36,11 +50,12 @@ const MetOffice = ({getTodaysWeather}) => {
     return(
         <>
             <div>
+                <button onClick={handleClick}>Re-render</button>
                 {dayCards}
             </div>
         </>
-    )
+    )}
 
-}
+// }
 
 export default MetOffice
